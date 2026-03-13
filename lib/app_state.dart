893def08748a86b2082782efe313b31cc5272ba0
Richard Spencer <richard.spencer@lifegroup.org.uk>
2026-03-13 10:31:43 +0000

    37 before move to mac

--- lib/app_state.dart
+++ lib/app_state.dart
@@ -12,7 +12,7 @@ import 'flutter_flow/flutter_flow_theme.dart';
 import 'dart:math';
 import 'appwrite_interface.dart';
 
-final int versionNumber = 36;
+final int versionNumber = 37;
 
 
 late SharedPreferences globalSharedPrefs;
