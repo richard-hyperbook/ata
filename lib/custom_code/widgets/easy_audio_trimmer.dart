1c8e57034eb0e9e21cd9a46b55d1e29b56b351e4
Richard Spencer <richard.spencer@lifegroup.org.uk>
2026-03-14 15:33:22 +0000

    39 changed to easy_audio_trimmer

---
+++ lib/custom_code/widgets/easy_audio_trimmer.dart
@@ -0,0 +1,12 @@
+library easy_audio_trimmer;
+
+
+export 'src/trimmer.dart';
+export 'src/utils/duration_style.dart';
+export 'src/utils/file_formats.dart';
+export 'src/utils/storage_dir.dart';
+export 'src/trim_viewer/trim_editor_properties.dart';
+export 'src/trim_viewer/trim_area_properties.dart';
+// Two types of trim viewers, the `TrimViewer` class helps to auto select
+// based on the length.
+export 'src/trim_viewer/trim_viewer.dart';
